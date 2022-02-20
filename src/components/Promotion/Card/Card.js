28e2550944f00de2ade50e2ca34acb977cc04b13
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2022-02-20 17:31:08 -0300

    Card component  full developed w/CSS

--- src/components/Promotion/Card/Card.js
+++ src/components/Promotion/Card/Card.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import './Card.css'
 
-const PromotionCard = ()=>{
+const PromotionCard = ({promo})=>{
   return (
-    <div>Hi</div>
+    <div className='promo-card' style={{
+      maxWidth:800,
+      margin:'30px auto'
+    }}>
+      <img className='promo-card__img' src={promo.imageUrl} alt='alt'/>
+      <div className='promo-card__info'>
+        <h1 className='promo-card__title'>{promo.title}</h1>
+        <span className='promo-card__price'>R$ {promo.price}</span>
+        <footer className='promo-card__footer'>
+          {promo.comments.length > 0 && (
+            <div className='promo-card__comment'>"{promo.comments[0].comment}"</div>
+          )}
+          <div className='promo-card__comments-count'>
+            {promo.comments.length}{' '}
+            {promo.comments.length>1 ? 'Comentários': 'Comentário'}
+          </div>
+          <a className='promo-card__link' href={promo.url} target='_blank' rel='noreferrer'>Ir para o site</a>
+        </footer>
+      </div>
+    </div>
   )
 }
 
